2b1b2896ef0fc2c31903e92cfcda0504ee205169
Zaid_ZiD <zid6177@gmail.com>
2022-07-01 02:40:33 +0500

    first weather app using an api

---
+++ app.js
@@ -0,0 +1,28 @@
+let data = () => {
+
+    let city = document.querySelector("#search").value;
+    axios.get(`https://api.weatherapi.com/v1/current.json?key=3a4c3daef759499091d212220223006&q=${city}`)
+        .then(function (response) {
+            // handle success
+            const data = response.data;
+
+            console.log(data);
+
+            let icon = data.current.condition.icon;
+            icon.replace("/file// ");
+            document.querySelector("#icon").src = icon;
+            document.querySelector("#temp_c").innerText = data.current.temp_c + "°C";
+            document.querySelector("#city").innerText = data.location.name
+            document.querySelector("#condition").innerText = data.current.condition.text;
+            document.querySelector("#feel").innerText = "Feels: " + data.current.feelslike_c + "°C";
+            document.querySelector("#feel2").innerText = "Feels: " + data.current.feelslike_f + "°F";
+            document.querySelector("#wind_kph").innerText = "Wind: " + data.current.wind_kph + "kph";
+            document.querySelector("#humidity").innerText = "Humidity: " + data.current.humidity + "%";
+            document.querySelector("#precip_in").innerText = "Precipitation: " + data.current.precip_in + "%";
+            document.querySelector("#visibility").innerText = "Visibility : " + data.current.vis_km + "km";
+
+
+        })
+
+}
+
